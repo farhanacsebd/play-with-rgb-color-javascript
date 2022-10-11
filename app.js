59ad6378e74c2f1d3c109044de55df64dbbe83da
farhanacsebd <farhana.cse.bd@gmail.com>
2022-10-11 23:57:24 +0600

    try to fixed rgb

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('hello');
+function getRandomRgb() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+  }
+  
+  for (var i = 0; i < 10; i++) {
+    getRandomRgb();
+  }
+
+document.getElementById('root').style.backgroundColor = color;
+document.getElementById('output').style.color = color;
+document.getElementById('output').innerText = color;
